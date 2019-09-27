37486b0561d1f5b9d34cc3fd9cd30c332e0b0657
Ariel Serranoni <arielserranoni@gmail.com>
2019-09-27 17:39:08 -0300

    adding gitinfo

--- auto/main.el
+++ auto/main.el
@@ -2,11 +2,11 @@
  "main"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
-                     '(("book" "12pt" "twoside" "a4paper")))
+                     '(("article" "12pt" "twoside" "a4paper")))
    (TeX-run-style-hooks
     "latex2e"
-    "book"
-    "bk12"
+    "article"
+    "art12"
     "packages"))
  :latex)
 
